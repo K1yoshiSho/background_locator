2ef6484a7e6fe63d415e21da6c2175fc802dec3a
Yelaman Yelmuratov <e99xxx@gmail.com>
2023-06-30 11:32:15 +0600

    first commit

---
+++ test/background_locator_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('background_locator_2');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel,
+        (MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  });
+}
